72a0215e3b9d36a21e5ee479a1138cbc473ffa5f
Shpendiiiii <shpendaliu69@gmail.com>
2023-12-25 22:47:16 +0100

    list comp part 2

--- app/Main.hs
+++ app/Main.hs
@@ -39,3 +39,40 @@ pM hasBags = case hasBags of
 
 --list comp
 lst = [ x * 2 | x <- [1..20] ]
+
+--list compt with a predicate 
+predicate_lst = [ x * 2 | x <- [1 ..10], x * 2 >= 10 ]
+ 
+--50 to 100, and take only those numbers when divided with 7, have the remainder 3
+specific_lst = [ x | x <- [50..100], x `mod` 7 == 3 ]
+
+--more predicates 
+more_pred = [ if x < 10 then "BOOM!" else "BANG!" | x <- [1..30], odd x]
+
+mp1 xs = [ x | x <- xs, odd x]
+
+boomBangs xs = [if x < 10 then "BANG!" else "BOOM!" | x <- xs, odd x]   
+
+
+--combine lists inside a list comp
+--e.g. two lists have each 3 elems, the resulting list will be 3^2
+cl = [x * y | x <- [1..3], y <- [1..2]]
+
+--combine lists, pt. 2 
+nouns = ["hobo","frog","pope"] 
+adjectives = ["lazy","grouchy","scheming"]
+funny_comb = [ a ++ " " ++ n | a <- adjectives, n <- nouns]
+
+removeNonUppercase xs = [c | c <- xs, c `elem` ['A'..'Z']]
+
+xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]] 
+
+removeOdd xxs = [[ x | x <- xs, even x]| xs <- xxs]
+
+showRes = removeOdd xxs
+
+
+--get the right triangle that gives the perimeter of 64
+triangles = [ (a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24]
+
+factorial n = product [1..n]
